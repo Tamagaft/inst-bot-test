6be2d07387bf48fe12dbfb7fa0de216b4b23d0d6
Tamagaft <ihorvoloshanovich@gmail.com>
2022-01-19 10:06:44 +0200

    gathering accounts by tag

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import random
 import time
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -73,4 +72,39 @@ class InstagramBot():
         browser.implicitly_wait(5)
         time.sleep(random.randrange(1, 3))
 
+    def get_tag_users(self, tag):
+        browser = self.browser
+
+        try:
+            browser.get(f"https://www.instagram.com/explore/tags/{tag}/")
+
+            print("Scrolling photos")
+            for i in range(5):
+                browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(random.randrange(2, 4))
+
+            print("getting posts links")
+            hrefs = browser.find_elements(By.TAG_NAME, 'a')
+            posts = [item.get_attribute('href') for item in hrefs if "/p/" in item.get_attribute('href')]
+
+            print("saving users")
+            print(len(posts))
+            users = set()
+            i = 0
+            for e in posts:
+                browser.get(e)
+                browser.implicitly_wait(5)
+                time.sleep(random.randrange(2, 4))
+
+                user = browser.find_element(By.CLASS_NAME, 'yWX7d')
+                users.add(user.text)
+
+            print(f"gathered {len(users)} users")
+
+            return users
+
+        except Exception as ex:
+            print(ex)
+        finally:
+            self.close_browser()
 
